a1dd949272d2100c376938f21cc301571245da57
dilli <drpaudel@gmail.com>
2014-04-15 16:23:31 -0700

    Converted followers reconstruction to mrjob

--- src/Preprocess/followers.py
+++ src/Preprocess/followers.py
@@ -1,25 +1,33 @@
+from mrjob.job import MRJob
+import json as simplejson
 
-import simplejson
+class MRFollowers(MRJob):
 
-f = open('tweets.txt', 'r')
+	def mapper_followers(self, _, line):
+		tweet = simplejson.loads(line)
+		retweetCount = 0
+		language = 'es'
+		if 'retweetCount' in tweet :
+			retweetCount = tweet['retweetCount']
+		if 'twitter_lang' in tweet :
+			language = tweet['twitter_lang']
 
-USERID_PREFIX = 'id:twitter.com:'
-for line in f :
-	if len(line) == 0 :
-		continue
+		if language == 'en' and retweetCount > 0:
+			body = tweet['object']['body']
+			tweeter = tweet['object']['actor']['id'][len('id:twitter.com:') :]
+			user = tweet['actor']['id'][len('id:twitter.com:') :]
+			yield user, tweeter
 
-	tweet = simplejson.loads(line)
-	retweetCount = 0
-	language = 'es'
-	if 'retweetCount' in tweet :
-		retweetCount = tweet['retweetCount']
-	if 'twitter_lang' in tweet :
-		language = tweet['twitter_lang']
+	
+	def reducer_followers(self, user, follows):
+		for u2 in follows:
+			yield user, u2
 
-	if language == 'en' and retweetCount > 0:
-		body = tweet['object']['body']
-		tweeter = tweet['object']['actor']['id'][len(USERID_PREFIX) :]
-		user = tweet['actor']['id'][len(USERID_PREFIX) :]
-		print user, tweeter
+	def steps(self):
+		return [
+			self.mr(mapper=self.mapper_followers,
+				reducer=self.reducer_followers)
+	    ]
 
-f.close()
+if __name__ == '__main__':
+    MRFollowers.run()
