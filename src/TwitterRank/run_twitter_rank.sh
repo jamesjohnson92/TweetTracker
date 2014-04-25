3791a0ba2c472c1211a1d53676601939ec8f90fb
Howon Lee <howon@howonlee.com>
2014-04-24 22:21:11 -0700

    Use --no-output in order to avoid stdout spam

--- src/TwitterRank/run_twitter_rank.sh
+++ src/TwitterRank/run_twitter_rank.sh
@@ -9,10 +9,10 @@ mrldajar='s3:///mrldajarbucket/Mr.LDA-0.0.1.jar';
 stopwords='s3:///mrldajarbucket/stopwords';
 setnums='--jobconf mapreduce.map.tasks=1 --jobconf mapreduce.reduce.tasks=1'
 
-python GenerateCorpus.py $setnums -r emr $indir --output-dir $outdir/corpus;
-python RunMrLDA.py $mrldajar $outdir $nummappers $numreducers $stopwords
-python FollowersTable.py  $setnums -r emr $indir --output-dir $outdir/followertable;
+python GenerateCorpus.py $setnums -r emr $indir --output-dir $outdir/corpus --no-output;
+python RunMrLDA.py emr $mrldajar $outdir $nummappers $numreducers $stopwords
+python FollowersTable.py  $setnums -r emr $indir --output-dir $outdir/followertable --no-output;
 python ldapreprocesspostprocess.py $numtopics; #run this in a post-script after FollowersTable
-python RunHive.py $outdir
-python GenerateGraph.py  $setnums -r emr $outdir/pregraph --numtopics $numtopics --sumgamma $outdir/gammasums/000000_0 --output-dir $outdir/graph;
-python GraphRank.py  $setnums -r emr $outdir/graph --output-dir $outdir/twitterrank;
+python RunHive.py emr $outdir
+python GenerateGraph.py  $setnums -r emr $outdir/pregraph --numtopics $numtopics --sumgamma $outdir/gammasums/000000_0 --output-dir $outdir/graph --no-output;
+python GraphRank.py  $setnums -r emr $outdir/graph --output-dir $outdir/twitterrank --no-output;
