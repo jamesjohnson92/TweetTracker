d6be6043da2c6ba21442a918fe2eb98d0655a5db
Howon Lee <howon@howonlee.com>
2014-05-03 18:48:01 -0700

    Fix the hdfs bug with embarrasing methods

--- src/TwitterRank/RunMrJobs.py
+++ src/TwitterRank/RunMrJobs.py
@@ -6,42 +6,47 @@ if __name__ == "__main__":
     if sys.argv[1] == "emr":
         conn = boto.emr.connect_to_region("us-east-1") #north virginia
         log_uri = u's3n://tweettrack/Twitterrank_Log/LDA_log'
-        mahoutjar = unicode(sys.argv[2])
+        mrjobsjar = unicode(sys.argv[2])
         outdir = unicode(sys.argv[3])
         nummappers = unicode(sys.argv[4])
         numreducers = unicode(sys.argv[5])
         numtopics = unicode(sys.argv[6])
         stopwords = unicode(sys.argv[7])
+        tempdir = unicode(sys.argv[8])
+        s3distcpjar = unicode(sys.argv[9])
 
-		jars = (s3distcpjar, mahoutjar, mahoutjar, mahoutjar, s3distcpjar)
-		class1 = u'stuff'
-        class2 = u'org.apache.mahout.text.SequenceFilesFromDirectory'
-        class3 = u'org.apache.mahout.vectorizer.SparseVectorsFromSequenceFiles'
-        class4 = u'org.apache.mahout.clustering.lda.cvb.CVB0Driver'
-		class5 = u'stuff'
-        classes = (class1, class2, class3, class4, class5)
-		args1 = []
+        jars = (s3distcpjar, mrjobsjar, mrjobsjar, mrjobsjar, s3distcpjar)
+        class2 = u'cc.mrlda.ParseCorpus'
+        class3 = u'cc.mrlda.VariationalInference'
+        class4 = u'cc.mrlda.DisplayDocument'
+        classes = (None, class2, class3, class4, None)
+        args1 = [u'--src', outdir + "corpus",
+                 u'--dest', tempdir + "corpus"]
         args2 = [
-                 u'--input', outdir + "corpus",
-                 u'--output', outdir + "seqfiles",
-                 u'-c', u'UTF-8'
-                ]
+                u'-input', tempdir + "corpus",
+                u'-output', tempdir + "parsecorpus",
+                u'-mapper', nummappers,
+                u'-reducer', numreducers,
+                u'-stoplist', stopwords]
         args3 = [
-                 u'--input', outdir + "seqfiles",
-                 u'--output', outdir + "vecs",
-                 u'-wt', u't'
-                ]
+                u'-input',  tempdir + "parsecorpus/document",
+                u'-output', tempdir + "ldapreout",
+                u'-mapper', nummappers,
+                u'-iteration', '5',
+                u'-reducer', numreducers,
+                u'-term', u'5000',
+                u'-topic', numtopics,
+                u'-directemit']
         args4 = [
-                 u'--input', outdir + "vecs",
-                 u'--output', outdir + "ldaout",
-                 u'--num_reduce_tasks', numreducers,
-                 u'--num_terms', u'10000',
-                 u'--maxIter', u'3000',
-                 u'--num_topics', numtopics]
-		args4 = []
+                u'-input',
+                tempdir + "ldapreout/gamma-5",
+                u'-output',
+                tempdir + "ldaout"]
+        args5 = [u'--src', tempdir + "ldaout",
+                 u'--dest', outdir + "ldaout"]
         args = (args1, args2, args3, args4, args5)
         steps = []
-        names = ("Copy to hdfs", "Convert to SequenceFile", "Sequence File to Parsed", "Mahout Job", "Copy from hdfs")
+        names = ("Copy to hdfs", "Parse Corpus", "Variational Inference", "Display Document", "Copy from hdfs")
         for i in xrange(5):
             step = boto.emr.JarStep(names[i],
                            jars[i],
@@ -51,8 +56,8 @@ if __name__ == "__main__":
             steps.append(step)
         master_instance_type = "m3.xlarge"
         slave_instance_type = "m3.xlarge"
-        num_instances = 2
-        jobid = conn.run_jobflow("Mahout", log_uri=log_uri,
+        num_instances = 7
+        jobid = conn.run_jobflow("Mr. LDA Risen From the Dead", log_uri=log_uri,
                                            steps=steps,
                                            master_instance_type=master_instance_type,
                                            slave_instance_type=slave_instance_type,
@@ -62,8 +67,10 @@ if __name__ == "__main__":
                                            hadoop_version="2.2.0")
         wait_until(lambda: check_connection(conn, jobid), 86400)
     else:
+        pass
         """
         Without elastic map reduce
+        """
         """
         for i in xrange(2, 8):
             assert sys.argv[i]
@@ -75,3 +82,4 @@ if __name__ == "__main__":
         stopwords = sys.argv[7]
         call(["hadoop", "jar", mahoutjar, "org.apache.mahout.clustering.lda.cvb"])
         ###figure this out later, params are fucked and other stuff
+        """
--- src/TwitterRank/run_twitter_rank.sh
+++ src/TwitterRank/run_twitter_rank.sh
@@ -1,19 +1,21 @@
 #!/bin/sh
 
-indir='s3n://tweettrack/Twitter_Teacup/';
+indir='s3n://tweettrack/Twitter_Thimble/';
 outdir='s3n://tweettrack/Twitterrank_Output/';
-temp_hdfs_dir='hdfs:///stuff';
+temphdfsdir='hdfs:///tweettracktemp/';
 numtopics=30;
 nummappers=3;
 numreducers=3;
-mrldajar='s3n://mrldajarbucket/mahout-examples-1.0-SNAPSHOT-job.jar'; ##mahout jar
+mahoutjar='s3n://mrldajarbucket/mahout-examples-1.0-SNAPSHOT-job.jar'; ##mahout jar
+mrldajar='s3n://mrldajarbucket/Mr.LDA-0.0.1.jar';
+s3distcpjar='/home/hadoop/lib/emr-s3distcp-1.0.jar' #cluster itself's home
 stopwords='s3n://mrldajarbucket/stopwords';
 setnums='--jobconf mapreduce.map.tasks=5 --jobconf mapreduce.reduce.tasks=5 --num-ec2-instances 6 --ec2-instance-type m3.xlarge'
 
 #python GenerateCorpus.py $setnums -r emr $indir --output-dir $outdir/corpus --no-output;
-python RunMahout.py emr $mrldajar $outdir $nummappers $numreducers $numtopics $stopwords #don't need no-output
+#python RunMrJobs.py emr $mrldajar $outdir $nummappers $numreducers $numtopics $stopwords $temphdfsdir $s3distcpjar;
 #python FollowersTable.py  $setnums -r emr $indir --output-dir $outdir/followertable | python ldapreprocesspostprocess.py $numtopics;
-#send to s3
-#python RunHive.py emr $outdir #don't need no-output
+#s3cmd put something something something0
+python RunHive.py emr $outdir $temphdfsdir $s3distcpjar;
 #python GenerateGraph.py  $setnums -r emr $outdir/pregraph --numtopics $numtopics --sumgamma $outdir/gammasums/000000_0 --output-dir $outdir/graph --no-output;
 #python GraphRank.py  $setnums -r emr $outdir/graph --output-dir $outdir/twitterrank --no-output;
