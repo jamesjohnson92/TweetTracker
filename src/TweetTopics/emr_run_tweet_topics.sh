31fa903284b205b398e22edde53594a3586b7881
Howon Lee <howon@howonlee.com>
2014-05-06 21:56:46 -0700

    begin the implementation of the emr-local split

--- src/TweetTopics/emr_run_tweet_topics.sh
+++ src/TweetTopics/emr_run_tweet_topics.sh
@@ -4,16 +4,16 @@ indir='hdfs:///user/cloudera/twitterdata/smallsample.txt';
 outdir='hdfs:///user/cloudera/tweettopicout';
 rankdir='hdfs:///user/cloudera/smallsampleout';
 numtopics=30;
-nummappers=1;
-numreducers=1;
+nummappers=3;
+numreducers=3;
 mrldajar='/home/cloudera/Mr.LDA/bin/Mr.LDA-0.0.1.jar'
+stopwords='s3n://mrldajarbucket/stopwords';
 
-#hadoop fs -rm -r $outdir;
-#hadoop fs -mkdir $outdir;
-
-#python GenerateTweetCorpus.py -r hadoop $indir --output-dir $outdir/corpus;
+#python GenerateTweetCorpus.py -r emr $indir --output-dir $outdir/corpus;
+python RunMrJobs.py emr $mrldajar $outdir $nummappers $numreducers $stopwords
 #hadoop jar $mrldajar cc.mrlda.ParseCorpus -index $rankdir'/parsecorpus/term' -input $outdir/corpus -output $outdir/parsecorpus -mapper $nummappers -reducer $numreducers -stoplist stopwords;
 #hadoop jar $mrldajar cc.mrlda.DisplayDocument -input $outdir/parsecorpus/document -output $outdir/wordcounts;
 #hadoop jar $mrldajar cc.mrlda.DisplayBeta -input $rankdir/ldapreout/beta-30 -output $outdir/wordprobs; 
 #hadoop jar $mrldajar cc.mrlda.DisplayPrior -input $rankdir/ldapreout/alpha-30 -output $outdir/priors; 
+python RunTweetTopicHive.py emr $outdir;
 hive -hiveconf TTPATH=$outdir -f tweettopichive.q;
