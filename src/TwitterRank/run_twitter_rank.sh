1878387b54602e562021c442d84dc72f02a0e2cb
Howon Lee <howon@howonlee.com>
2014-05-06 17:49:21 -0700

    finish one iteration today

--- src/TwitterRank/run_twitter_rank.sh
+++ src/TwitterRank/run_twitter_rank.sh
@@ -17,5 +17,5 @@ setnums='--jobconf mapreduce.map.tasks=5 --jobconf mapreduce.reduce.tasks=5 --nu
 #python RunMrJobs.py emr $mrldajar $outdir $nummappers $numreducers $numtopics $stopwords $temphdfsdir $s3distcpjar;
 #python FollowersTable.py  $setnums -r emr $indir --output-dir $outdir/followertable ###create the ldapostprocess now
 #python RunHive.py emr $outdir;
-python GenerateGraph.py  $setnums -r emr $outdirnoslash/pregraph/ --numtopics $numtopics --sumgamma $outdirnoslash/gammasums --output-dir $outdirnoslash/graph --enable-emr-debugging; ##this is very fragile
-#python GraphRank.py  $setnums -r emr $outdir/graph --output-dir $outdir/twitterrank --no-output;
+#python GenerateGraph.py  $setnums -r emr $outdirnoslash/pregraph/ --numtopics $numtopics --sumgamma $outdirnoslash/gammasums/80bc4c70-e2c5-46b9-83fe-aa458371a2a2_000000 --output-dir $outdirnoslash/graph --enable-emr-debugging --no-output; ##this is very fragile, do the gammasums right
+python GraphRank.py  $setnums -r emr $outdirnoslash/graph/ --output-dir $outdirnoslash/twitterrank --enable-emr-debugging --no-output;
