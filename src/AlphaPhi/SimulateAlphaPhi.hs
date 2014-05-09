245bf7ac2afd82e1dfa0b2fb906a35fefd308ba9
Joseph Victor <jvictor@stanford.edu>
2014-05-09 12:48:57 -0700

    I just realized I'm an idiot.  After conditioning on the topic, the estimation problem splits as a coproduct, so for the sake of solving the estimation problem the topics effectively don't matter. I removed them from the simulation model, had the phis be beta distributed (flat-ish).

--- src/AlphaPhi/SimulateAlphaPhi.hs
+++ src/AlphaPhi/SimulateAlphaPhi.hs
@@ -12,12 +12,12 @@ import Data.Random.Distribution.ChiSquare
 import Data.Random.Distribution.Dirichlet
 import Data.RVar
 import Data.Tuple.HT
+import Data.Random
 
-type Graph = Array Int (Array Int Double, [Int]) 
+type Graph = Array Int (Double, [Int]) 
 
 type Entry = (Int, -- tweet_id
              Int, -- user_id
-             Int, -- tau
              Bool) -- was retweeted
 
 type GraphMetadata = Array Int (Double,Double,Double) -- we generate alpha from beta distribution with the first two params
@@ -31,45 +31,44 @@ generateMetadata num_users = do
     beta <- sampleRVar $ chiSquare 5 :: IO Double
     return (i,(alpha,beta,prob))
 
-readGraph :: Int -> FilePath -> IO Graph
-readGraph num_topics filename = do
+readGraph :: FilePath -> IO Graph
+readGraph filename = do
   mat <- fmap (map words . lines) $ readFile filename
   let edgeLists = map (\l -> map fst $ filter ((=="1").snd) $ zip [1..] l) mat
   fmap (array (1, length mat)) $ forM (zip [1..] edgeLists) $ \(i,es) -> do
-    phi <- fmap (array (1,num_topics) . zip [1..num_topics] ) $ sampleRVar $ dirichlet $ replicate num_topics 0.1
+    phi <- sampleRVar $ beta 1.25 1.25 :: IO Double
     return (i,(phi,es))
     
-type SimConsts = (Int,Double,Int,Graph) -- (tweet,alpha,tau,graph)
+type SimConsts = (Int,Double,Graph) -- (tweet,alpha,tau,graph)
 
 simulateAlphaPhi :: Int -> Graph -> GraphMetadata -> IO [Entry]
 simulateAlphaPhi num_tweets graph metadata = do
-  let tweeterDist = categorical $ map (\(i,(_,_,p)) -> (p,i)) $ assocs metadata
+  let tweeterDist = categorical $ map (\(i,(_,_,p)) -> (p,i)) $ assocs metadata :: RVar Int
   fmap concat $ forM [1..num_tweets] $ \tweet_id -> do
-    tweeter <- sampleRVar tweeterDist
-    topic <- sampleRVar $ categorical $ map swap $ assocs $ fst $ graph!tweeter
+    tweeter <- sampleRVar tweeterDist :: IO Int
     alpha <- sampleRVar $ beta (fst3 $ metadata!tweeter) (snd3 $ metadata!tweeter)
-    simulateTweet tweet_id alpha topic tweeter graph
+    simulateTweet tweet_id alpha tweeter graph
 
-simulateTweet :: Int -> Double -> Int -> Int -> Graph -> IO [Entry]
-simulateTweet tweet alpha tau poster graph = do
+simulateTweet :: Int -> Double -> Int -> Graph -> IO [Entry]
+simulateTweet tweet alpha poster graph = do
   seen <- newIORef $ Set.singleton poster
-  simulateTweet_ (tweet,alpha,tau,graph) poster seen
+  simulateTweet_ (tweet,alpha,graph) poster seen
 
-simulateTweet_ cs@(tweet,alpha,tau,graph) node seen = do
+simulateTweet_ cs@(tweet,alpha,graph) node seen = do
   pre_seen <- readIORef seen
   modifyIORef seen $ \s -> foldl' (flip Set.insert) s $ snd $ graph!node
   fmap concat $ forM (filter (not . flip Set.member pre_seen) $ snd $ graph!node) $ \n -> do
     p <- randomIO :: IO Double
-    if p < alpha * ((fst $ graph!n)!tau)
-      then fmap ((tweet,n,tau,True):) $ simulateTweet_ cs n seen
-      else return [(tweet,n,tau,False)]
+    if p < alpha * (fst $ graph!n)
+      then fmap ((tweet,n,True):) $ simulateTweet_ cs n seen
+      else return [(tweet,n,False)]
            
 toInt False = 0
 toInt True = 1
            
 doit = do
-  graph <- readGraph 10 "jomat"
+  graph <- readGraph "jomat"
   let (1,n) = bounds graph
   md <- generateMetadata n
   res <- simulateAlphaPhi 1000 graph md 
-  writeFile "sap_out" $ unlines $ flip map res $ \(a,b,c,d) -> unwords $ map show [a,b,c,toInt d]
+  writeFile "sap_out" $ unlines $ flip map res $ \(a,b,c) -> unwords $ map show [a,b,toInt c]
