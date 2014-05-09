ac8c14864637d8b90b18f8ad4a36e5c877b1124e
JoYo <jvictor@stanford.edu>
2014-05-08 18:06:11 -0700

    THe last push was premature...

--- src/Preprocess/MRRetweetRate2.py
+++ src/Preprocess/MRRetweetRate2.py
@@ -1,16 +1,20 @@
 from mrjob.job import MRJob
 import json as simplejson
+from mrjob.protocol import RawValueProtocol
 from datetime import datetime, timedelta
 
 class MRRetweetRate(MRJob):
 	
 
-    PARTITION_SECS = 60*5 # bin things in five minutes
+    OUTPUT_PROTOCOL = RawValueProtocol
+
+
+    PARTITION_SECS = 60 # bin things in five minutes
     NUM_PARTITIONS = 60*60/PARTITION_SECS # record for an hour
 
     def mapper(self, _, line):
         startTime = datetime.strptime('2014-03-01T00:00:00.000Z', '%Y-%m-%dT%H:%M:%S.%fZ') 
-        endTime = datetime.strptime('2014-03-01T20:00:00.000Z', '%Y-%m-%dT%H:%M:%S.%fZ') 
+        endTime = datetime.strptime('2014-03-01T02:00:00.000Z', '%Y-%m-%dT%H:%M:%S.%fZ') 
         tweet = simplejson.loads(line)
         if 'twitter_lang' in tweet : 
             language = tweet['twitter_lang']
@@ -20,6 +24,7 @@ class MRRetweetRate(MRJob):
                     retweetTime = datetime.strptime(tweet['postedTime'], '%Y-%m-%dT%H:%M:%S.%fZ')
                     creationTime = retweetTime
                     favoritesCount = tweet['favoritesCount']
+                    retweetCount = tweet['retweetCount']
                     if tweet['retweetCount'] > 0 :
                         tweetid = tweet['object']['id'][len('object:search.twitter.com,2005:') :]
                         creationTime = datetime.strptime(tweet['object']['postedTime'], '%Y-%m-%dT%H:%M:%S.%fZ')
@@ -28,19 +33,20 @@ class MRRetweetRate(MRJob):
                         
                     if creationTime >= startTime and creationTime <= endTime :
                         timeDelta = retweetTime - creationTime
-                        yield tweetid, (int((timeDelta.days * 86400 + timeDelta.seconds)), favoritesCount)
+                        yield tweetid, (int((timeDelta.days * 86400 + timeDelta.seconds)), retweetCount, favoritesCount)
                             
     def reducer(self, tweetid, timestamps):
+
         rt_rates = [0] * self.NUM_PARTITIONS
         fv_rates = [0] * self.NUM_PARTITIONS
-        for t, fc in timestamps :
+        for t, rc, fc in timestamps :
             bin = t / self.PARTITION_SECS;
-            if bin < len(rt_rates):
-                rt_rates[bin] = rt_rates[bin] + 1
-                for b in xrange(bin,len(rates)):
-                    fv_rates[b] = max(fv_rates[b], fc)
+            for b in xrange(bin,self.NUM_PARTITIONS):
+                fv_rates[b] = max(fv_rates[b], fc)
+                rt_rates[b] = max(rt_rates[b], rc)
+                
             
-        yield None, '%s %s ' % (' '.join(map(lambda x : str(x[0]), rates)),' '.join(map(lambda x : str(x[1]), rates)))
+        yield None, '%s %s %s ' % (str(tweetid), ' '.join(map(str, rt_rates)),' '.join(map(str, fv_rates)))
             
 
 if __name__ == '__main__':
