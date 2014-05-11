21d4d507393a879fb23314134e74bf5c13ff8d17
dilli <drpaudel@gmail.com>
2014-05-11 10:39:50 -0700

    compute edge weights based on breakouts

--- src/Preprocess/MRRetweetRate.py
+++ src/Preprocess/MRRetweetRate.py
@@ -7,35 +7,71 @@ class MRRetweetRate(MRJob):
 	def mapper_get_times(self, _, line):
 		startTime = datetime.strptime('2014-03-01T00:00:00.000Z', '%Y-%m-%dT%H:%M:%S.%fZ') 
 		tweet = simplejson.loads(line)
+		language = 'es'
 		if 'twitter_lang' in tweet : 
 			language = tweet['twitter_lang']
-			if language == 'en' :
-				if 'id' in tweet and 'retweetCount' in tweet :
-					tweetid = tweet['id'][len('tag:search.twitter.com,2005:') : ]
-					retweetTime = datetime.strptime(tweet['postedTime'], '%Y-%m-%dT%H:%M:%S.%fZ')
-					creationTime = retweetTime
-					if tweet['retweetCount'] > 0 :
-						tweetid = tweet['object']['id'][len('object:search.twitter.com,2005:') :]
-						creationTime = datetime.strptime(tweet['object']['postedTime'], '%Y-%m-%dT%H:%M:%S.%fZ')
 
-					if creationTime >= startTime :
-						timeDelta = retweetTime - creationTime
-						yield tweetid, int((timeDelta.days * 86400 + timeDelta.seconds)/300)
+		if language == 'en' :
+			if 'id' in tweet and 'retweetCount' in tweet :
+				tweetid = tweet['id'][len('tag:search.twitter.com,2005:') : ]
+				tweeter = tweet['actor']['id'][len('id:twitter.com:') :]
+				retweetTime = datetime.strptime(tweet['postedTime'], '%Y-%m-%dT%H:%M:%S.%fZ')
+				creationTime = retweetTime
+				if tweet['retweetCount'] > 0 :
+					tweetid = tweet['object']['id'][len('object:search.twitter.com,2005:') :]
+					creationTime = datetime.strptime(tweet['object']['postedTime'], '%Y-%m-%dT%H:%M:%S.%fZ')
+
+				if (creationTime - startTime).total_seconds() > 0 :
+					timeDelta = (retweetTime - startTime).total_seconds() - (creationTime - startTime).total_seconds()	
+					yield tweetid, (str(tweeter), int(timeDelta/300))
 				
 	def reducer_compute_rate(self, tweetid, timestamps):
-		timeIndexes = [int(t) for t in timestamps]
+		timeIndexes = []
+		users = []
+		for (u, t) in timestamps : 
+			timeIndexes.append(t)
+			users.append(u)
+
 		maxTimeIndex = max(timeIndexes)
 		rates = [0 for i in xrange(maxTimeIndex + 1)]
 		for t in timeIndexes :
 			rates[t] = rates[t] + 1
 
-		yield str(tweetid), '%s ' % ' '.join(map(str, rates))
-		# the space at the end is important!!!! #blackmagic		
+		start = 0
+		breakout = 1
+		for r in rates :
+			if r < start or r < 5:
+				breakout = 0
+				break
+			start = r
+
+		if breakout == 1 :
+			yield tweetid, ' '.join(map(str, rates))
+			# the space at the end is important!!!! #blackmagic		
+
+	def reducer_edgeweights(self, user, counts):
+		tweets = 0
+		retweetCounts = 0
+		retweets = {}
+		for c in counts :
+			if c == "T" :
+				tweets = tweets + 1
+			else :
+				retweetCounts = retweetCounts + 1
+				if c in retweets :
+					retweets[c] = retweets[c] + 1
+				else :
+					retweets[c] = 1
+
+		yield user, (str(tweets), str(retweetCounts))
+		for k, v in retweets.items() :
+			yield user, (k, str(v))
 
 	def steps(self):
 		return [
 			self.mr(mapper=self.mapper_get_times,
 				reducer=self.reducer_compute_rate)
+		#	self.mr(reducer=self.reducer_edgeweights)
 		]
 
 if __name__ == '__main__':
--- src/Preprocess/retweetrate.py
+++ src/Preprocess/retweetrate.py
@@ -2,8 +2,8 @@
 from datetime import datetime, timedelta
 import simplejson
 
-f = open('tweets.txt', 'r')
-startTime = datetime.strptime('2014-03-01T00:00:00.000Z', '%Y-%m-%dT%H:%M:%S.%fZ') 
+f = open('data/tweets1.txt', 'r')
+startTime = datetime.strptime('2014-03-01T00:00:00.000Z', '%Y-%m-%dT%H:%M:%S.%fZ')
 
 retweetRates = {}
 for line in f :
@@ -13,20 +13,64 @@ for line in f :
 		if language == 'en' :
 			if 'id' in tweet and 'retweetCount' in tweet :
 				tweetid = tweet['id'][len('tag:search.twitter.com,2005:') : ]
+				tweeter = tweet['actor']['id'][len('id:twitter.com:') :]
 				retweetTime = datetime.strptime(tweet['postedTime'], '%Y-%m-%dT%H:%M:%S.%fZ')
 				creationTime = retweetTime
 				if tweet['retweetCount'] > 0 :
 					tweetid = tweet['object']['id'][len('object:search.twitter.com,2005:') :]
 					creationTime = datetime.strptime(tweet['object']['postedTime'], '%Y-%m-%dT%H:%M:%S.%fZ')
 
-				if creationTime >= startTime :
-					timeDelta = retweetTime - creationTime
+				if (creationTime - startTime).total_seconds() > 0 :
+					timeDelta = (retweetTime - startTime).total_seconds() - (creationTime - startTime).total_seconds()
+					print timeDelta
 					rates = []
 					if tweetid in retweetRates :
 						rates = retweetRates[tweetid]
 					
-					rates.append(int((timeDelta.days * 86400 + timeDelta.seconds)/300))
+					rates.append((tweeter, int(timeDelta/300)))
 					retweetRates[tweetid] = rates
 
+breakoutTweetCounts = {}
+breakoutRetweetCounts = {}
+
 for k, v in retweetRates.items() :
-	print k, ' '.join(map(str, v))
+	timeIndexes = [t for (u,t) in v]
+	users = [u for (u, t) in v]
+	maxTimeIndex = max(timeIndexes)
+	rates = [0 for i in xrange(maxTimeIndex + 1)]
+	for t in timeIndexes :
+		rates[t] = rates[t] + 1
+
+	start = 0
+	breakout = 1
+	for r in rates :
+		if r < start or r < 5:
+			breakout = 0
+			break
+		start = r
+
+	if breakout == 1 :
+		tweeter = users[0]
+		if tweeter in breakoutTweetCounts :
+			breakoutTweetCounts[tweeter] = breakoutTweetCounts[tweeter] + 1
+		else :
+			breakoutTweetCounts[tweeter] = 1
+
+		for i in range(1, len(users)) :
+			u = users[i]
+			if u in breakoutRetweetCounts :
+				breakoutRetweetCounts[u] = breakoutRetweetCounts[u] + 1
+			else :
+				breakoutRetweetCounts[u] = 1
+
+
+for k, v in breakoutTweetCounts.items() :
+	stat = str(k) + ', ' + str(v)
+	if k in breakoutRetweetCounts :
+		stat = stat + ', ' + str(breakoutRetweetCounts[k])
+
+	print stat
+
+for k, v in breakoutRetweetCounts.items() :
+	if k not in breakoutTweetCounts :
+		print str(k) + ', , ' + str(v)
