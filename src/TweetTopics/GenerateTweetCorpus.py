ec75eaae9e9b70635a5aef60c297e81ccfaaca61
Joseph Victor <jvictor@stanford.edu>
2014-05-13 16:57:04 -0700

    Modified corpus generators to remove @ mentionsa and double-count #-tags while removing the tag in front

--- src/TweetTopics/GenerateTweetCorpus.py
+++ src/TweetTopics/GenerateTweetCorpus.py
@@ -5,19 +5,30 @@ TWEETID_PREFIX = 'tag:search.twitter.com,2005:'
 
 class GenerateTweetCorpus(MRJob):
 
+    OUTPUT_PROTOCOL = RawValueProtocol
+
     def mapper(self, _, line):
         tweet = simplejson.loads(line)
         if 'twitter_lang' in tweet and tweet['twitter_lang'] == 'en':
             tweet_id = tweet['id'][len(TWEETID_PREFIX) :]
+
+            the_tweet = tweet['body'].split(' ')
+            result = []
+            for i in xrange(len(the_tweet)):
+                if not the_tweet[i].startswith('@') && the_tweet[i] != 'RT' && not the_tweet[i].startswith('#'):
+                    result.append(the_tweet[i].lower())
+                if the_tweet[i].startswith('#'):
+                    result.append(the_tweet[i][1:].lower())
+                    result.append(the_tweet[i][1:].lower())
+
             if 'retweetCount' in tweet and int(tweet['retweetCount']) > 0:
                 tweet_id = tweet['object']['id'][len(TWEETID_PREFIX) :]
-            yield int(tweet_id), tweet['body']
+            yield int(tweet_id), ' '.join(the_tweet)
 
     def reducer(self, key, vals):
         for v in vals:
-            yield key, v
+            yield None, "%d %s" % (key, v)
             return #uniqueification
 
 if __name__ == '__main__':
     GenerateTweetCorpus.run()
-
--- src/TwitterRank/GenerateCorpus.py
+++ src/TwitterRank/GenerateCorpus.py
@@ -5,17 +5,28 @@ USERID_PREFIX = 'id:twitter.com:'
 
 class GenerateCorpus(MRJob):
 
+    MAPPER_OUTPUT_PROTOCOL = RawValueProtocol
+    OUTPUT_PROTOCOL = RawValueProtocol
+
     def mapper(self, _, line):
         tweet = simplejson.loads(line)
         if 'twitter_lang' in tweet and tweet['twitter_lang'] == 'en' and 'actor' in tweet :
             user = tweet['actor']['id'][len(USERID_PREFIX) :]
-            yield int(user), tweet['body']
+            the_tweet = tweet['body'].split(' ')
+            result = []
+            for i in xrange(len(the_tweet)):
+                if not the_tweet[i].startswith('@') && the_tweet[i] != 'RT' && not the_tweet[i].startswith('#'):
+                    result.append(the_tweet[i].lower())
+                if the_tweet[i].startswith('#'):
+                    result.append(the_tweet[i][1:].lower())
+                    result.append(the_tweet[i][1:].lower())
+            yield int(user), ' '.join(result)
             if 'retweetCount' in tweet and int(tweet['retweetCount']) > 0:
                 tweeter = tweet['object']['actor']['id'][len(USERID_PREFIX) :]
-                yield int(tweeter), tweet['body']
+                yield int(tweeter), ' '.join(the_tweet)
 
     def reducer(self, user, tweets):
-        yield user, ' '.join(tweets)
+        yield None, "%s %s" % (user,' '.join(tweets))
 
 
 if __name__ == '__main__':
