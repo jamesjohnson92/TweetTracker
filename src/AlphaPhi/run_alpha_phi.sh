532612db04c9e02108bfc207a9ea7650db182e2f
Joseph Victor <jvictor@stanford.edu>
2014-05-16 09:43:29 -0700

    Fixing GenerateCorpus for stupid

--- src/AlphaPhi/run_alpha_phi.sh
+++ src/AlphaPhi/run_alpha_phi.sh
@@ -9,10 +9,10 @@ s3distcpjar='/home/hadoop/lib/emr-s3distcp-1.0.jar' #cluster itself's home
 setnums='--jobconf mapreduce.map.tasks=5 --jobconf mapreduce.reduce.tasks=5 --num-ec2-instances 6 --ec2-instance-type m3.xlarge'
 
 #python TweetTable.py -r emr $indir/ --output-dir $outdir/tweettable --no-output;
-python RunAlphaPhiHive.py emr $outdir $topicdir $numtopics
+#python RunAlphaPhiHive.py emr $outdir $topicdir $numtopics
 
-#for i in `seq 1 $numtopics`
-#do
-#    python AlphaFile.py $setnums -r emr $outdir/alphaphiin$i/ --mode 'alpha' --output-dir $outdir/alphafile$i/ --no-output
-#    python AlphaFile.py $setnums -r emr $outdir/alphaphiin$i/ --mode 'phi' --output-dir $outdir/phifile$i/ --no-output
-#done
+for i in `seq 1 $numtopics`
+do
+    python AlphaFile.py $setnums -r emr $outdir/alphaphiin$i/ --mode 'alpha' --output-dir $outdir/alphafile$i/ --no-output
+    python AlphaFile.py $setnums -r emr $outdir/alphaphiin$i/ --mode 'phi' --output-dir $outdir/phifile$i/ --no-output
+done
--- src/TwitterRank/GenerateCorpus.py
+++ src/TwitterRank/GenerateCorpus.py
@@ -1,32 +1,75 @@
 from mrjob.job import MRJob
+from mrjob.protocol import RawValueProtocol
 import json as simplejson
+from stemming.porter2 import stem
+import enchant
+import string
 
 USERID_PREFIX = 'id:twitter.com:'
 
 class GenerateCorpus(MRJob):
 
-    MAPPER_OUTPUT_PROTOCOL = RawValueProtocol
     OUTPUT_PROTOCOL = RawValueProtocol
+
+    def small_edit_distance(self,w1,w2,ed_max):
+        if ed_max < 0:
+            return False
+        if w1 == "":
+            return len(w2) <= ed_max
+        if w2 == "":
+            return len(w1) <= ed_max
+        if w1[0] == w1[0]:
+            return self.small_edit_distance(w1[1:],w2[1:],ed_max)
+        else:
+            return self.small_edit_distance(w1[1:],w2,ed_max-1) or self.small_edit_distance(w1,w2[1:],ed_max-1) or self.small_edit_distance(w1[1:],w2[1:],ed_max-1) 
+
+    def prepare_word(self,word):
+        if word.startswith('@'):
+            return False
+        if word.startswith('#'):
+            res = self.prepare_word(word[1:])
+            if res:
+                return ' '.join([res,res])
+
+        if len(word) == 0:
+            return False
+
+        word = word.encode('ascii','ignore').translate(None,string.punctuation).lower()
+
+        if len(word) == 0:
+            return False
+        
+        if self.dict.check(word):
+            return stem(word)
+
+        suggestions = self.dict.suggest(word)
+        if len(suggestions) > 0:
+            new_word = suggestions[0].lower()
+            if self.small_edit_distance(new_word,word,2):
+                return stem(new_word)
+        return False
+
+    
+    def mapper_init(self):
+        self.dict = enchant.Dict("en_US")
 
     def mapper(self, _, line):
         tweet = simplejson.loads(line)
         if 'twitter_lang' in tweet and tweet['twitter_lang'] == 'en' and 'actor' in tweet :
             user = tweet['actor']['id'][len(USERID_PREFIX) :]
-            the_tweet = tweet['body'].split(' ')
+            the_tweet = tweet['body'].split()
             result = []
             for i in xrange(len(the_tweet)):
-                if not the_tweet[i].startswith('@') && the_tweet[i] != 'RT' && not the_tweet[i].startswith('#'):
-                    result.append(the_tweet[i].lower())
-                if the_tweet[i].startswith('#'):
-                    result.append(the_tweet[i][1:].lower())
-                    result.append(the_tweet[i][1:].lower())
+                word = self.prepare_word(the_tweet[i])
+                if word:
+                    result.append(word)
             yield int(user), ' '.join(result)
             if 'retweetCount' in tweet and int(tweet['retweetCount']) > 0:
                 tweeter = tweet['object']['actor']['id'][len(USERID_PREFIX) :]
-                yield int(tweeter), ' '.join(the_tweet)
+                yield int(tweeter), ' '.join(result)
 
     def reducer(self, user, tweets):
-        yield None, "%s %s" % (user,' '.join(tweets))
+        yield None, "%s\t%s" % (user,' '.join(tweets))
 
 
 if __name__ == '__main__':
