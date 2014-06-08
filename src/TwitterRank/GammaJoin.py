5cb9b55c49d89b880b76dc5bae7d08d27c6b9b26
Howon Lee <howon@howonlee.com>
2014-06-08 13:53:15 -0700

    not not working version of GammaJoin

--- src/TwitterRank/GammaJoin.py
+++ src/TwitterRank/GammaJoin.py
@@ -30,63 +30,67 @@ def read_followertable(folder_name, bucket):
     return followertable
 
 def key_iterator(key):
-	"""
-	Iterator for line by line, for going through the whole contents of a key
-	"""
+    """
+    Iterator for line by line, for going through the whole contents of a key
+    """
     unfinished_line = ""
     for byte in key:
         byte = unfinished_line + byte
-        lines = byte_split("\n")
+        lines = byte.split("\n")
         unfinished_line = lines.pop()
         for line in lines:
             yield line
 
 def get_gammas(folder_name, bucket):
-	"""
-	This is barely kosher because ldaout is 4.5 gigs
-	Use a big fucking instance, accept that you are going to run out of memory
-	"""
+    """
+    This is barely kosher because ldaout is 4.5 gigs
+    Use a big fucking instance, accept that you are going to run out of memory
+    """
     keys = bucket.list(folder_name)
-	gammas = {}
-	for f in keys:
+    gammas = {}
+    count = 0
+    for f in keys:
         print f
         f_iter = key_iterator(f)
-		for line in f_iter:
+        for line in f_iter:
+            count += 1
             split_line = line.split()
             if split_line and len(split_line) == 31:
-				gammas[str(split_line[0])] = split_line[1:]
+                gammas[str(split_line[0])] = split_line[1:]
+            if count % 50000 == 0:
+                print "count is now : %d" % (count,)
     return gammas
 
 def do_join(output_folder_name, followertable, gammas, bucket):
-	"""
-	Goes through follower gammas and does the join, saves it to remote in real time like
-	"""
+    """
+    Goes through follower gammas and does the join, saves it to remote in real time like
+    """
     buf = []
-	count = 0
-	save_size = 50000000
-	for followtup, tweet_count in followertable.iteritems():
-		tup = tuple(followtup)
-		friend_id = int(tup[0])
-		follower_id = int(tup[1])
-		if friend_id in gammas and follower_id in gammas:
-			count += 1
-			toJoin = []
-			toJoin.append(str(follower_id))
-			toJoin = toJoin + gammas[follower_id]
-			toJoin.append(str(friend_id))
-			toJoin.append(tweet_count)
-			toJoin = toJoin + gammas[friend_id]
-			joined_line = " ".join(toJoin)
-			buf.append(joined_line)
-			if count % save_size == 0:
-				output_name = output_folder_name + str(count // save_size)
-				print "output name is: %s " % (output_name,)
-				print "here's a thing from the buffer: %s", buf[1]
-				#dry run
-				#new_file = bucket.new_key(output_name)
-				#new_file.set_contents_from_string("\n".join(buf))
-				print "saving... %d, total size of file is %d" % (count // save_size, f_size)
-				buf = []
+    count = 0
+    save_size = 50000000
+    for followtup, tweet_count in followertable.iteritems():
+        tup = tuple(followtup)
+        friend_id = int(tup[0])
+        follower_id = int(tup[1])
+        if friend_id in gammas and follower_id in gammas:
+            count += 1
+            toJoin = []
+            toJoin.append(str(follower_id))
+            toJoin = toJoin + gammas[follower_id]
+            toJoin.append(str(friend_id))
+            toJoin.append(tweet_count)
+            toJoin = toJoin + gammas[friend_id]
+            joined_line = " ".join(toJoin)
+            buf.append(joined_line)
+            if count % save_size == 0:
+                output_name = output_folder_name + str(count // save_size)
+                print "output name is: %s " % (output_name,)
+                print "here's a thing from the buffer: %s", buf[1]
+                #dry run
+                #new_file = bucket.new_key(output_name)
+                #new_file.set_contents_from_string("\n".join(buf))
+                print "saving... %d, total size of file is %d" % (count // save_size, f_size)
+                buf = []
 
 def main(args):
     """
@@ -106,7 +110,7 @@ def main(args):
     else:
         with open(sys.argv[4], "r") as followertable_file:
             followertable = simplejson.load(followertable_file)
-	gammas = get_gammas(args[2], bucket)
+    gammas = get_gammas(args[2], bucket)
     do_join(args[3], followertable, gammas, bucket)
     conn.close()
 
